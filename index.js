cd688775a24d504913840cc138856587f6e5f8e6
or-obaidullah <obaidullah.cse.diit@gmail.com>
2021-09-12 00:58:11 +0600

    update something

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const pass = "Burjarabian2021"
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fi07c.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fi07c.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 client.connect(err => {
   const bookings = client.db("burjAlArab").collection("bookings");
